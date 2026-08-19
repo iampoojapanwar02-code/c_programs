846cd14fd0fde210d7f982e7ef56bbed2d42a135
iampoojapanwar02-code <iampoojapanwar02@gmail.com>
2026-08-19 15:40:04 +0530

    sum of two

---
+++ simple_programs/sum_of_two.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,sum;
+	printf("Enter a:");
+	scanf("%d",&a);
+	printf("Enter b:");
+	scanf("%d",&b);
+	printf("Sum of two number is %d",sum);
+	return 0;
+}
